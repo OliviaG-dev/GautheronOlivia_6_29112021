205d50c739a78fd2071ed6d3b7398bbdd3d484a5
OliviaG-dev <oxtramag@gmail.com>
2021-12-31 12:56:07 +0100

    lightbox Css/html and WIP js

--- scripts/factories/gallery.js
+++ scripts/factories/gallery.js
@@ -14,6 +14,7 @@ export function galleryFactory(data) {
     //media
     //const a = document.createElement("a");
     
+//////////////////////////////////////////////////////////////////////////////////
     let media;
     if (data.hasOwnProperty("image")) {
       link = `./assets/photo/${photographerId}/${image}`;
@@ -30,11 +31,15 @@ export function galleryFactory(data) {
     media.appendChild(source);
     }
     media.addEventListener("click", function(e){
+		e.preventDefault() 
+		//e.target.getAttribute("data-link") 
+		 
       console.log(e.target);
       console.log(e.target.getAttribute("data-link"));
       //alert(e.target.getAttribute("data-link"))
     })
-    
+////////////////////////////////////////////////////////////////////////////////////
+
     media.setAttribute("data-link", link)
 
     const div = document.createElement("div");
@@ -68,5 +73,44 @@ export function galleryFactory(data) {
     return article;
   }
 
-  return { id, likes, date, title, getGalleryDOM };
-}
+  function getLightboxDOM(){ 
+ 
+	const lightbox = createElement("article") 
+	lightbox.className = "lightbox"; 
+	 
+		const close = createElement("boutton") 
+		close.className = "lightbox-close"; 
+	 
+		const next = createElement("boutton") 
+		next.className = "lightbox-next"; 
+	 
+		const prev = createElement("button") 
+		prev.className = "ligthbox-prev"; 
+		 
+		const container = createElement("div") 
+		container.className = "ligthbox-container"; 
+	 
+		const picture = createElement("img") 
+		picture.setAttribute("data-link", link); 
+	 
+		lightbox.appendChild("lightbox-close"); 
+		lightbox.appendChild("lightbox-next"); 
+		lightbox.appendChild("lightbox-prev"); 
+		lightbox.appendChild("lightbox-container"); 
+		container.appendChild("picture"); 
+	 
+		return lightbox; 
+		// <!-- <article class="lightbox"> 
+		// 	  <button class="lightbox-close">Fermer</button> 
+		// 	  <button class="lightbox-next">Suivant</button> 
+		// 	  <button class="lightbox-prev">Précédent</button> 
+		// 	  <div class="lightbox-container"> 
+		// 		  <img src="./assets/photo/925/Fashion_Wings.jpg" alt=""> 
+		// 	  </div> 
+		//   </article> --> 
+	} 
+	 
+	return { id, likes, date, title, getGalleryDOM ,getLightboxDOM }; 
+	} 
+	
+
